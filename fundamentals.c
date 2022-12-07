77771cbff06447fe6e7d7285954fce0d7ce41128
WalkerCity <119983186+ElioseAbrahams@users.noreply.github.com>
2022-12-06 20:01:03 -0500

    CPR project 2 initial commit
    Pushed v2 updates to fundamentals and manipulations

--- fundamentals.c
+++ fundamentals.c
@@ -6,6 +6,7 @@
 void fundamentals(void) // is the opening of ccde as fundamentals is the class
 {
 	/* Version 1 */
+	
 	printf("*** Start of Indexing Strings Demo ***\n"); //This line displays the start in the consale
 	char buffer1[BUFFER_SIZE]; //this line sets the buffer1 as an arrary of BUFFER_SIZE in char
 	char numInput[NUM_INPUT_SIZE]; //this line sets the numInput as an arrary of NUM_INPUT_SIZE in char
@@ -13,15 +14,15 @@ void fundamentals(void) // is the opening of ccde as fundamentals is the class
 	do // is a do-while loop
 	{
 		printf("Type not empty string (q - to quit):\n"); // tells user enter a string or to enter q to quit
-		fgets(buffer1, BUFFER_SIZE, stdin); //gets is a stdlib function used to take input from user and it reads the string buffer1 and the array BUFFER_SIZE inside the string then stores its respective string value 
-		buffer1[strlen(buffer1) - 1] = '\0';  // strlen determines the length of the string buffer1 and asks the user for 
-		if (strcmp(buffer1, "q") != 0) // this checks if only q is enterted 
+		fgets(buffer1, BUFFER_SIZE, stdin); //gets is a stdlib function used to take input from user and it reads the string buffer1 and the array BUFFER_SIZE inside the string then stores its respective string value
+		buffer1[strlen(buffer1) - 1] = '\0';  // strlen determines the length of the string buffer1 and asks the user for
+		if (strcmp(buffer1, "q") != 0) // this checks if only q is enterted
 		{
 			printf("Type the character position within the string:\n"); // tells user to enter a position
 			fgets(numInput, NUM_INPUT_SIZE, stdin); // takes the input from user and reads the NUM_INPUT_SIZE
 			numInput[strlen(numInput) - 1] = '\0'; // numInput is has numInput - 1
-			position = atoi(numInput); //position is the string of numInput 
-			if (position >= strlen(buffer1)) // if the position is greater then or = to buffer1 do what is in the breakets 
+			position = atoi(numInput); //position is the string of numInput
+			if (position >= strlen(buffer1)) // if the position is greater then or = to buffer1 do what is in the breakets
 			{
 				position = strlen(buffer1) - 1; // postion returns its length of buffer1 string
 				printf("Too big... Position reduced to max. availbale\n"); //tells the if it is over the buffer size of 80
@@ -30,9 +31,23 @@ void fundamentals(void) // is the opening of ccde as fundamentals is the class
 				(int)position, buffer1[position]); // prints the character position and what it is
 		}
 	} while (strcmp(buffer1, "q") != 0); // if q is enterted it quits the program
-	printf("*** End of Indexing Strings Demo ***\n\n");// Marks the end of the Demo 
+	printf("*** End of Indexing Strings Demo ***\n\n");// Marks the end of the Demo
+	
 
 	/* Version 2 */
+	printf("*** Start of Measuring Strings Demo ***\n"); //This line displays the start in the consale
+	char buffer2[BUFFER_SIZE]; //this line sets the buffer2 as an arrary of BUFFER_SIZE in char
+	do // is a do-while loop
+	{
+		printf("Type a string (q - to quit):\n"); // tells user enter a string or to enter q to quit
+		fgets(buffer2, BUFFER_SIZE, stdin); // reads the strings and what size
+		buffer2[strlen(buffer2) - 1] = '\0';  // strlen determines the length of the string buffer2 and asks the user for 
+		if (strlen(buffer2, "q") != 0) // checks if q is pressed and if not do what is in the brakets 
+			printf("The length of \'%s\' is %d characters\n",
+				buffer2, (int)strlen(buffer2)); // prints the string and number of charaters  
+	} while (strcmp(buffer2, "q") != 0); // if buffer2 is q then stop the loop
+	printf("*** End of Measuring Strings Demo ***\n\n");// marks the end of the demo
+
 
 
 	/* Version 3 */
--- manipulating.c
+++ manipulating.c
@@ -1,30 +1,54 @@
-#define _CRT_SECURE_NO_WARNINGS // makes sure there are no uneeded 
+#define _CRT_SECURE_NO_WARNINGS // makes sure there are no uneeded errors
 #define BUFFER_SIZE 80 // sets BUFFER_SIZE with up to varbarbles 80 
 #include "manipulating.h" // includes manipulating.h
 
 void manipulating(void) { // is the class
 	/* Version 1 */
-	printf("*** Start of Concatenating Strings Demo ***\n"); // starting line 
+	
+	printf("*** Start of Concatenating Strings Demo ***\n"); // starting line
 	char string1[BUFFER_SIZE]; // sets string1 in bounds of BUFFER_SIZE
 	char string2[BUFFER_SIZE]; // sets string2 in bounds of BUFFER_SIZE
 	do
 	{
 		printf("Type the 1st string (q - to quit):\n"); // ask user for the first string and what to put to quit
-		fgets(string1, BUFFER_SIZE, stdin); // reads the strings and what size 
+		fgets(string1, BUFFER_SIZE, stdin); // reads the strings and what size
 		string1[strlen(string1) - 1] = '\0'; // counts the first sting
-		if ((strcmp(string1, "q") != 0)) // compares the string to check if q is entered once 
+		if ((strcmp(string1, "q") != 0)) // compares the string to check if q is entered once
 		{
 			printf("Type the 2nd string:\n"); // ask user for the second string
-			fgets(string2, BUFFER_SIZE, stdin); // reads the strings and what size 
+			fgets(string2, BUFFER_SIZE, stdin); // reads the strings and what size
 			string2[strlen(string2) - 1] = '\0'; // it counts second string2
 			strcat(string1, string2); // Puts both strings togather next to each other
 			printf("Concatenated string is \'%s\'\n", string1); // prints the strings together
 		}
 	} while (strcmp(string1, "q") != 0); // if string1 has just q it quits the program
 	printf("*** End of Concatenating Strings Demo ***\n\n");// marks the end of the demo
-
+	
 	/* Version 2 */
-
+	printf("*** Start of Comparing Strings Demo ***\n"); // starting line 
+	char compare1[BUFFER_SIZE]; // sets compare1 in bounds of BUFFER_SIZE
+	char compare2[BUFFER_SIZE]; // sets compare1 in bounds of BUFFER_SIZE
+	int result; // makes result in int
+	do
+	{
+		printf("Type the 1st string to compare (q - to quit):\n"); // ask user for the first string and what to put to quit
+		fgets(compare1, BUFFER_SIZE, stdin); // reads the strings and what size
+		compare1[strlen(compare1) - 1] = '\0'; //  takes compare1 and returns its lenght
+		if (strlen(compare1, "q") != 0) // checks if q is pressed and if not do what is in the brakets 
+		{
+			printf("Type the 2nd string to compare:\n"); // ask user for the second string
+			fgets(compare2, BUFFER_SIZE, stdin); // reads the strings and what size
+			compare2[strlen(compare2) - 1] = '\0'; //  takes compare2 and returns its lenght
+			result = strcmp(compare1, compare2); // it compares compare1 string and compare2 string by each character
+			if (result < 0) // if result is less then 0 do what is under 
+				printf("\'%s\' string is less than \'%s\'\n", compare1, compare2); // tells user that string 1 is less then string 2
+			else if (result == 0) // if result is equal to 0 do what is under 
+				printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2);  // tells user that string 1 is equal to string 2
+			else // if it is not the what is above do what is under
+				printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2); // tells user that string 1 is greater then string 2
+		}
+	} while (strcmp(compare1, "q") != 0); // if compare1 is q then stop the loop
+	printf("*** End of Comparing Strings Demo ***\n\n");// marks the end of the demo
 
 
 	/* Version 3 */
